6d27f1ea6fec57aded81b215ccde7dc241bba533
Gurudutt Suryanarayana <gnsuryan@users.noreply.github.com>
2020-07-17 15:24:01 +0530

    Update installNginx.sh

--- src/main/scripts/installNginx.sh
+++ src/main/scripts/installNginx.sh
@@ -1,42 +1,72 @@
 #!/bin/bash
 
-nginx -v
+function checkIfNginxInstalled()
+{
+    nginx -v
 
-if [ $? == 0 ];
-then
-  echo "Nginx already installed"
-  exit 0
-fi
+    if [ $? == 0 ];
+    then
+      echo "Nginx already installed"
+      startNginx
+      verifyNginxRunning
+      exit 0
+    fi
+}
 
+function installNginx()
+{
+  touch /etc/yum.repos.d/nginx.repo
+
+  cat << 'EOF' > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/rhel/7/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+  sudo yum -y install nginx
+
+  nginx -v
+
+  if [ $? == 0 ];
+  then
+    echo "Nginx installed successfully"
+  else
+    echo "Nginx installation unsuccessfull"
+    exit 1
+  fi
+
+}
+
+function startNginx()
+{
+  sudo systemctl start nginx
+  sudo firewall-cmd --permanent --zone=public --add-service=http 
+  sudo firewall-cmd --permanent --zone=public --add-service=https
+  sudo firewall-cmd --reload
+}
+
+function verifyNginxRunning()
+{
+  curl -L -s http://localhost/
+
+  retCode=$(curl -L -s -o /dev/null -w "%{http_code}" http://localhost/)
+
+  if [ "$retCode" == "200" ];
+  then
+    echo "Nginx installed and started successfully"
+  else
+    echo "Nginx installation is successful, but couldn't start Nginx. Please check the logs and try again."
+    exit 1
+  fi
+}
+
+
+#main
+
+checkIfNginxInstalled
 echo "Nginx not installed. Installing Now..."
-
-sudo yum install epel-release
-sudo yum install nginx
-
-nginx -v
-
-if [ $? == 0 ];
-then
-  echo "Nginx installed successfully"
-else
-  echo "Nginx installation unsuccessfull"
-  exit 1
-fi
-
-sudo systemctl start nginx
-
-sudo firewall-cmd --permanent --zone=public --add-service=http 
-sudo firewall-cmd --permanent --zone=public --add-service=https
-sudo firewall-cmd --reload
-
-curl -L -s http://localhost/
-
-retCode=$(curl -L -s -o /dev/null -w "%{http_code}" http://localhost/)
-
-if [ "$retCode" == "200" ];
-then
-  echo "Nginx installed and started successfully"
-else
-  echo "Nginx installation is successful, but couldn't start Nginx. Please check the logs and try again."
-  exit 1
-fi
+installNginx
+startNginx
+verifyNginxRunning
